505f3508303bb6bc6a33e1194c73d80846ec1d5c
faoxis <faoxis@gmail.com>
2018-04-24 07:31:34 +0300

    init

---
+++ client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+
+MAX_CONNECTIONS = 10
+address_to_server = ('localhost', 8686)
+
+clients = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(MAX_CONNECTIONS)]
+for client in clients:
+    client.connect(address_to_server)
+
+for i in range(MAX_CONNECTIONS):
+    clients[i].send(bytes("hello from client number " + str(i), encoding='UTF-8'))
+
+for client in clients:
+    data = client.recv(1024)
+    print(str(data))
+
